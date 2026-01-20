8dfd46b3795b3d59dcd326f2b9f2b6acebe236df
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2026-01-20 16:18:37 +0000

    Initial inclusion of api_key and gemini call

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+
 def main():
-    print("Hello from recipe-vision-engine!")
+    generated_content = client.models.generate_content(
+        model="gemini-3-flash-preview",
+        contents="Explain how AI works in a few words"
+    )
+    
+    print(generated_content.text)
+    
 
 
 if __name__ == "__main__":
