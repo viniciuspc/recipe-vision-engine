03d998f35426500fe240ccdaf2f22858636a5542
viniciuspc <pasqualini9@gmail.com>
2026-01-24 12:54:07 +0000

    Treat the exception more speficifically

--- functions/gemini/call_ai_agent.py
+++ functions/gemini/call_ai_agent.py
@@ -3,6 +3,7 @@ import pathlib
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from google.genai.errors import ClientError
 from prompts import system_prompt
 from functions.tools.call_function import available_functions, call_function
 
@@ -54,9 +55,9 @@ def ai_agent_loop(client, name, contents, verbose):
                   return
           else:
               raise RuntimeError("Failed to connect to Gemini API")
-      except Exception as e:
+      except ClientError as e:
           print(f"Error processing recipe {name}: {e}")
-          continue
+          return
   
   print("Model could not produce a result with 20 interation. Exiting with code 1")
 
@@ -79,4 +80,59 @@ def call_ai_agent(name, files, verbose):
 
     ai_agent_loop(client, name, contents, verbose)
     
-    
+#def test():
+    # for _ in range(20):
+  #     try:
+  #         generated_content = client.models.generate_content(
+  #             model="gemini-3-flash-preview",
+  #             contents=contents,
+  #             config=types.GenerateContentConfig(
+  #                 tools=[available_functions], system_instruction=system_prompt
+  #             )
+  #         )
+
+  #         if verbose:
+  #            print(f"Generate content: {generated_content}")
+
+  #         usage_metadata = generated_content.usage_metadata    
+  #         if usage_metadata:
+  #           if verbose:
+  #             print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
+  #             print(f"Response tokens: {usage_metadata.candidates_token_count}")
+          
+  #           cadidates = generated_content.candidates
+  #           if cadidates:
+  #               for candidate in cadidates:
+  #                   parts = candidate.content.parts
+  #                   function_results = []
+  #                   if parts:
+  #                     for part in parts:
+                          
+  #                         function_call = part.function_call
+
+  #                         if function_call:
+
+  #                           function_call_result = call_function(function_call, verbose)
+                          
+  #                           if not function_call_result.parts:
+  #                               raise Exception("function_call_result.parts does not exists ")
+                            
+  #                           function_call_result_part = function_call_result.parts[0]
+                            
+  #                           if not function_call_result_part.function_response or not function_call_result_part.function_response.response:
+  #                               raise Exception("function_call_result response does not exists")
+                            
+  #                           function_results.append(function_call_result_part)
+                            
+  #                           if verbose:
+  #                               print(f"-> {function_call_result.parts[0].function_response.response}")
+  #                         else:
+  #                           print(part.generated_text)
+  #                           return
+
+  #                   contents.append(candidate.content)
+  #                   if(function_results):
+  #                     contents.append(types.Content(role="user", parts=function_results))
+
+  #           else:
+  #             raise RuntimeError("Failed to connect to Gemini API")
